49038788649620e7cd464c484b404bf23461760d
h-xgs <2864948230@qq.com>
2023-05-22 09:59:25 +0800

    [CustomDialogDemo]更新AlertDialogUtil
    Signed-off-by: h-xgs <2864948230@qq.com>

--- CustomDialogDemo/src/main/java/com/android/customdialogdemo/AlertDialogUtil.java
+++ CustomDialogDemo/src/main/java/com/android/customdialogdemo/AlertDialogUtil.java
@@ -48,10 +48,18 @@ public class AlertDialogUtil {
             TextView dialogMessage = view.findViewById(R.id.dialog_message);
             Button cancelButton = view.findViewById(R.id.dialog_cancel_button);
             Button okButton = view.findViewById(R.id.dialog_ok_button);
-            dialogTitle.setText(title);
-            dialogMessage.setText(msg);
-            cancelButton.setText(cancelButtonText);
-            okButton.setText(okButtonText);
+            if (title != null) {
+                dialogTitle.setText(title);
+            }
+            if (msg != null) {
+                dialogMessage.setText(msg);
+            }
+            if (cancelButtonText != null) {
+                cancelButton.setText(cancelButtonText);
+            }
+            if (okButtonText != null) {
+                okButton.setText(okButtonText);
+            }
             cancelButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
